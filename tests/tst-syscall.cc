5556ea3d274d771006e0bb6577e5db78522fde0c
fatiao <fatiao@in66.com>
2018-07-09 18:06:23 +0800

    a wrapper for syscall

--- tests/tst-syscall.cc
+++ tests/tst-syscall.cc
@@ -114,6 +114,132 @@ uint64_t* CtnPtr(T *b){
 #define SysCallCmd5 SysCallCmd4 "movq %6, %%r8\n\t"
 #define SysCallCmd6 SysCallCmd5 "movq %7, %%r9\n\t"
 
+#define SysCallRet "movq %%rax,%0\n\t"
+
+#define OutputReg(x) "=r"(x)
+#define InputReg(x) "r"(x)
+#define SysCallInRegList0(nr,ofs) InputReg(nr) 
+#define SysCallInRegList1(nr,a1) InputReg(nr),InputReg(a1)
+#define SysCallInRegList2(nr,a1,a2) SysCallInRegList1(nr,a1),InputReg(a2)
+#define SysCallInRegList3(nr,a1,a2,a3) SysCallInRegList2(nr,a1,a2),InputReg(a3)
+#define SysCallInRegList4(nr,a1,a2,a3,a4) SysCallInRegList3(nr,a1,a2,a3),InputReg(a4)
+#define SysCallInRegList5(nr,a1,a2,a3,a4,a5) SysCallInRegList4(nr,a1,a2,a3,a4),InputReg(a5)
+#define SysCallInRegList6(nr,a1,a2,a3,a4,a5,a6) SysCallInRegList5(nr,a1,a2,a3,a4,a5),InputReg(a6)
+
+#define SysCallCrobList0 "%rax"
+#define SysCallCrobList1 SysCallCrobList0,"%rdi"
+#define SysCallCrobList2 SysCallCrobList1,"%rsi"
+#define SysCallCrobList3 SysCallCrobList2,"%rdx"
+#define SysCallCrobList4 SysCallCrobList3,"%r10"
+#define SysCallCrobList5 SysCallCrobList4,"%r8"
+#define SysCallCrobList6 SysCallCrobList5,"%r9"
+
+
+#define SysCallTemplateX8664(ret, NR, i, ...) do { asm( \
+        SysCallCmd##i    \
+        "syscall\n\t"   \
+        SysCallRet      \
+        :OutputReg(ret) \
+        :SysCallInRegList##i(NR, __VA_ARGS__) \
+        :SysCallCrobList##i \
+        ); } while(0)
+
+#define X86_64DefSysCall0(ret, NR)          SysCallTemplateX8664(ret, NR, 0)
+#define X86_64DefSysCall1(ret, NR, a1)      SysCallTemplateX8664(ret, NR, 1, a1)
+#define X86_64DefSysCall2(ret, NR, a1, a2)  SysCallTemplateX8664(ret, NR, 2, a1, a2)
+
+#define X86_64DefSysCall3(ret, NR, a1, a2, a3) \
+    SysCallTemplateX8664(ret, NR, 3, a1, a2, a3)
+#define X86_64DefSysCall4(ret, NR, a1, a2, a3, a4) \
+    SysCallTemplateX8664(ret, NR, 4, a1, a2, a3, a4)
+#define X86_64DefSysCall5(ret, NR, a1, a2, a3, a4, a5) \
+    SysCallTemplateX8664(ret, NR, 5, a1, a2, a3, a4, a5)
+#define X86_64DefSysCall6(ret, NR, a1, a2, a3, a4, a5, a6) \
+    SysCallTemplateX8664(ret, NR, 6, a1, a2, a3, a4, a5, a6)
+
+
+int DoSysCall(uint64_t SysCallNR)
+{
+    uint64_t ret;
+    X86_64DefSysCall0(ret, SysCallNR);
+    return (int)ret;
+}
+
+template<typename T1>
+int DoSysCall(uint64_t SysCallNR, T1 a1)
+{
+    uint64_t ret;
+    b8 x1;
+    makeb48(&x1, &a1);
+    X86_64DefSysCall1(ret, SysCallNR, (*CtnPtr(&x1)));
+    return (int)ret;
+}
+
+template<typename T1, typename T2>
+int DoSysCall(uint64_t SysCallNR, T1 a1, T2 a2)
+{
+    uint64_t ret;
+    b8 x1, x2;
+    makeb48(&x1, &a1);
+    makeb48(&x2, &a2);
+    X86_64DefSysCall2(ret, SysCallNR, (*CtnPtr(&x1)), (*CtnPtr(&x2)));
+    return (int)ret;
+}
+
+template<typename T1, typename T2, typename T3>
+int DoSysCall(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3)
+{
+    uint64_t ret;
+    b8 x1, x2, x3;
+    makeb48(&x1, &a1);
+    makeb48(&x2, &a2);
+    makeb48(&x3, &a3);
+    X86_64DefSysCall3(ret, SysCallNR, (*CtnPtr(&x1)), (*CtnPtr(&x2)), (*CtnPtr(&x3)));
+    return (int)ret;
+}
+
+template<typename T1, typename T2, typename T3, typename T4>
+int DoSysCall(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3, T4 a4)
+{
+    uint64_t ret;
+    b8 x1, x2, x3, x4;
+    makeb48(&x1, &a1);
+    makeb48(&x2, &a2);
+    makeb48(&x3, &a3);
+    makeb48(&x4, &a4);
+    X86_64DefSysCall4(ret, SysCallNR, (*CtnPtr(&x1)), (*CtnPtr(&x2)), (*CtnPtr(&x3)), (*CtnPtr(&x4)));
+    return (int)ret;
+}
+
+template<typename T1, typename T2, typename T3, typename T4, typename T5>
+int DoSysCall(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5)
+{
+    uint64_t ret;
+    b8 x1, x2, x3, x4, x5;
+    makeb48(&x1, &a1);
+    makeb48(&x2, &a2);
+    makeb48(&x3, &a3);
+    makeb48(&x4, &a4);
+    makeb48(&x5, &a5);
+    X86_64DefSysCall5(ret, SysCallNR, (*CtnPtr(&x1)), (*CtnPtr(&x2)), (*CtnPtr(&x3)), (*CtnPtr(&x4)), (*CtnPtr(&x5)));
+    return (int)ret;
+}
+
+template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+int DoSysCall(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3, T4 a4, T5 a5, T6 a6)
+{
+    uint64_t ret;
+    b8 x1, x2, x3, x4, x5, x6;
+    makeb48(&x1, &a1);
+    makeb48(&x2, &a2);
+    makeb48(&x3, &a3);
+    makeb48(&x4, &a4);
+    makeb48(&x5, &a5);
+    makeb48(&x6, &a6);
+    X86_64DefSysCall6(ret, SysCallNR, (*CtnPtr(&x1)), (*CtnPtr(&x2)), (*CtnPtr(&x3)), (*CtnPtr(&x4)), (*CtnPtr(&x5)), (*CtnPtr(&x6)));
+    return (int)ret;
+}
+
 template<typename T1, typename T2, typename T3>
 int DoSysCall0(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3)
 {
@@ -146,7 +272,15 @@ int DoSysCall0(uint64_t SysCallNR, T1 a1, T2 a2, T3 a3)
 
 
 int Write(int fd, const char* str, int num) {
-    return DoSysCall0(SYS_write, fd, str, num);
+    return DoSysCall(SYS_write, fd, str, num);
+}
+
+int Exit(int code) {
+    return DoSysCall(SYS_exit, code);
+}
+
+int Gettid() {
+    return DoSysCall(SYS_gettid);
 }
 
 int main() {
@@ -154,5 +288,7 @@ int main() {
     const char * str1 = "Hello Syscall\n";
     ret = Write(1, str1, strlen(str1));
     std::cout << "Ret: " << ret << std::endl;
+
+    std::cout << "Tid: " << Gettid() << std::endl;
     return 0;
 }
